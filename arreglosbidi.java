1f04c2ec94a2073d2c58b8dc5a9de65516cf026a
dankar12 <143622228+dankar12@users.noreply.github.com>
2023-11-07 21:02:50 -0600

    Add files via upload

---
+++ arreglosbidi.java
@@ -0,0 +1,23 @@
+
+public class arreglosbidi {
+	
+	public static void main(String[] args) {
+		char[][] arreglo = new char[3][5];
+        char letra = 'a';
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+                arreglo[i][j] = letra;
+                letra++;
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(arreglo[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+	
